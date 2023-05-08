a86032965f6e55f61f5a7695dafe8a8b95f7b85d
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2023-05-07 22:34:10 -0600

    first commit: getting back into the depths of react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/Welcome';
+import Header from './components/Header';
+import Shop from './components/Shop';
+import Gallery from './components/Gallery';
+import {useState} from 'react';
 
 function App() {
+  const [activePage, setActivePage] = useState('welcome');
+
+  const changePage = (page) => {
+    setActivePage(page);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {activePage !== 'welcome' && <Header />}
+      {activePage === 'welcome' && <Welcome changePage={changePage} />}
+      {activePage === 'shop' && <Shop />}
+      {activePage === 'gallery' && <Gallery />}
     </div>
   );
 }
